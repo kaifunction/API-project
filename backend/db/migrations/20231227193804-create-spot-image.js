44712789588ae05be81b246874efaca0561d7099
Kai <fengkai0129@gmail.com>
2023-12-29 13:57:14 -0800

    add allowNull:false to FK

--- backend/db/migrations/20231227193804-create-spot-image.js
+++ backend/db/migrations/20231227193804-create-spot-image.js
@@ -18,7 +18,8 @@ module.exports = {
         references: {
           model: 'Spots',
           key: 'id'
-        }
+        },
+        onDelete: 'CASCADE'
       },
       url: {
         type: Sequelize.STRING(255)
