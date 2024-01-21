852b88920c5a492b022f54a814b157b6dfb1ff33
Kai <fengkai0129@gmail.com>
2024-01-21 12:28:59 -0800

    Airbnb Logo

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -36,7 +36,7 @@ function Navigation({ isLoaded }) {
   return (
     <ul>
       <li>
-        <NavLink to="/">Home</NavLink>
+        <NavLink to="/"><img src='https://upload.wikimedia.org/wikipedia/commons/6/69/Airbnb_Logo_B%C3%A9lo.svg' alt='Home'/></NavLink>
       </li>
       {isLoaded && sessionLinks}
     </ul>
