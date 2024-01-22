fefef7fb392038d7a00cb0fbb0d2589ccf9176cb
Kai <fengkai0129@gmail.com>
2024-01-21 23:27:02 -0800

    login layout

--- frontend/src/components/SignupFormPage/SignupFormPage.jsx
+++ frontend/src/components/SignupFormPage/SignupFormPage.jsx
@@ -41,7 +41,7 @@ function SignupFormPage() {
   };
 
   return (
-    <>
+    <div className='signupContainer'>
       <h1>Sign Up</h1>
       <form onSubmit={handleSubmit}>
         <label>
@@ -106,7 +106,7 @@ function SignupFormPage() {
         {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
         <button type="submit">Sign Up</button>
       </form>
-    </>
+    </div>
   );
 }
 
